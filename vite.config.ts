2fb9551bda84f3f3ba4a399a15339cd75c481120
Nathan <Nathanhsiao123@gmail.com>
2025-11-10 17:16:01 -0600

    Set Vite base to /Flappy-Bird/ for Github Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,19 +4,23 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ mode }) => {
+  const repoName = "Flappy-Bird"; // <- replace with your GitHub repo name
+  return ({
+    base: mode === "development" ? "/" : `/${repoName}/`,
+    server: {
+      host: "::",
+      port: 8080,
     },
-  },
-}));
+    plugins: [
+      react(),
+      mode === 'development' &&
+      componentTagger(),
+    ].filter(Boolean),
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+  })
+});
